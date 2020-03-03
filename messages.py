9e85848c7c261fee2058c31ba75250589821f59c
Kmiokande <brunnodesouzabezerra@gmail.com>
2020-03-03 00:00:21 -0300

    Adicionando um arquivo com as menssagens

---
+++ messages.py
@@ -0,0 +1,14 @@
+RULES = (
+    "[ 1 ] - Não haver discriminação com religião, raça e sexo.\n"
+    "[ 2 ] - Não discutir política, religião e futebol.\n"
+    "[ 3 ] - Não enviar links maliciosos ou encurtados.\n"
+    "[ 4 ] - Não compartilhar arquivos apk, zip e rar.\n"
+    "[ 5 ] - Proibido compartilhar fotos ou vídeos de acidentes, "
+    "pornografia e informações que não seja de interesse do grupo.\n"
+    "[ 6 ] - Proibido mensagem de cunho comercial, "
+    "venda de produtos ou serviço.\n"
+    "[ 7 ] - Não compartilhar conteúdo sem autorização "
+    "ou qualquer tipo que se encaixe como pirataria.\n"
+    "[ 8 ] - Não ficar conversando com o bot no grupo.\n"
+    "[ 9 ] - Proibido enviar spam.\n"
+)
