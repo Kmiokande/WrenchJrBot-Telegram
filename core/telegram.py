1666d342076e003bb136c4d3042c748cbfc7ebcc
Kmiokande <brunnodesouzabezerra@gmail.com>
2020-03-02 23:19:55 -0300

    Adicionando a instancia do core

--- core/telegram.py
+++ core/telegram.py
@@ -13,11 +13,20 @@ logger = logging.getLogger(__name__)
 
 
 class TelegramCore(ABC):
+    def __new__(cls, *args, **kwargs):
+        if not cls._instance:
+            TelegramCore._instance = super().__new__(cls)
+        return cls._instance
+
     def __init__(self):
         logging.info('Ligando...')
         self._token = config('BOT_TOKEN')
         self._updater = Updater(self._token, use_context=True)
         self._running = False
+
+    @classmethod
+    def instance(cls):
+        return cls._instance or cls()
 
     def run(self):
         """Start the bot as a python script loop"""
