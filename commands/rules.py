f07ab3534369bdbeee869cdd4352884e81fa7113
Kmiokande <brunnodesouzabezerra@gmail.com>
2020-03-03 00:02:31 -0300

    Excluindo o messages.py

--- commands/rules.py
+++ commands/rules.py
@@ -1,7 +1,6 @@
 import logging
 from telegram.ext import CommandHandler
 from core import TelegramCore
-from messages import RULES
 
 
 # Enable logging
@@ -13,7 +12,19 @@ logger = logging.getLogger(__name__)
 
 
 def rules(update, context):
-    update.message.reply_text(RULES)
+    update.message.reply_text(
+        f"[ 1 ] - Não haver discriminação com religião, raça e sexo.\n"
+        f"[ 2 ] - Não discutir política, religião e futebol.\n"
+        f"[ 3 ] - Não enviar links maliciosos ou encurtados.\n"
+        f"[ 4 ] - Não compartilhar arquivos apk, zip e rar.\n"
+        f"[ 5 ] - Proibido compartilhar fotos ou vídeos de acidentes, "
+        f"pornografia e informações que não seja de interesse do grupo.\n"
+        f"[ 6 ] - Proibido mensagem de cunho comercial, "
+        f"venda de produtos ou serviço.\n"
+        f"[ 7 ] - Não compartilhar conteúdo sem autorização "
+        f"ou qualquer tipo que se encaixe como pirataria.\n"
+        f"[ 8 ] - Não ficar conversando com o bot no grupo.\n"
+        f"[ 9 ] - Proibido enviar spam.\n")
 
 
 def config_handlers(instance: TelegramCore):
