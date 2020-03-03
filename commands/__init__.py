e922a3a68a78cc167b03be22f7020a8178f4315b
Kmiokande <brunnodesouzabezerra@gmail.com>
2020-03-03 11:09:27 -0300

    Adicionando boas vindas a um novo membro do grupo

--- commands/__init__.py
+++ commands/__init__.py
@@ -1,10 +1,12 @@
 from commands.start import config_handlers as start_handler
+from commands.welcome import config_handlers as welcome_handler
 from commands.about import config_handlers as about_handler
 from commands.rules import config_handlers as rules_handler
 from commands.crypto import config_handlers as crypto_handler
 
 handlers = [
     start_handler,
+    welcome_handler,
     about_handler,
     rules_handler,
     crypto_handler
