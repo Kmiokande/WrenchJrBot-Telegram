e685cc21ecc84f365e61dec74f3eefa5f0bbf681
Kmiokande <brunnodesouzabezerra@gmail.com>
2020-03-02 23:25:00 -0300

    Fazendo a chamada dos comandos no objeto

--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
 import logging
 from decouple import config
 from core import TelegramCore
+from commands import handlers
 
 # Enable logging
 logging.basicConfig(
@@ -13,6 +14,13 @@ logger = logging.getLogger(__name__)
 class WrenchJr(TelegramCore):
     def __init__(self):
         super(WrenchJr, self).__init__()
+        self._handlers_configured = False
+        self.config_handlers()
+
+    def config_handlers(self):
+        for config_handler in handlers:
+            config_handler(self)
+        self._handlers_configured = True
 
 
 if __name__ == '__main__':
