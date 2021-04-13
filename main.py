06c6ba3ad8eca638be0faba547698abe77ddde21
Kmiokande <brunnodesouzabezerra@gmail.com>
2021-04-13 03:06:40 -0300

    Adicionando webhook

--- main.py
+++ main.py
@@ -21,6 +21,9 @@ logger = logging.getLogger(__name__)
 
 def main():
     token = config('BOT_TOKEN')
+    mode = config('MODE', default='cmd')
+    name = config('SERVER_NAME')
+    port = config('PORT', default=8443, cast=int)
     updater = Updater(token=token, use_context=True)
     dispatcher = updater.dispatcher
 
@@ -36,7 +39,16 @@ def main():
 
     dispatcher.add_error_handler(error_handler)
 
-    updater.start_polling()
+    if mode == "cmd":
+        updater.start_polling()
+    elif mode == "web":
+        updater.start_webhook(listen="0.0.0.0",
+                          port=int(port),
+                          url_path=token)
+        updater.bot.setWebhook("https://{}.herokuapp.com/{}".format(name, token))
+    else:
+        raise Exception("O modo passado não foi reconhecido")
+
     logger.info('Listening humans as %s..' % updater.bot.username)
     updater.idle()
     logger.info('Bot stopped gracefully')
